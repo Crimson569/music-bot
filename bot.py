f9d2ce7f4e476a1e351dfcc4d83a530d172a16b5
Tvarkovskiy_Nickolay <80266130+Crimson569@users.noreply.github.com>
2024-12-05 21:35:30 +0200

    Добавлен поиск по названию музыки

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+import requests
 
 API_TOKEN = '7822913089:AAHC2dPksVN_I6keo-x7vbIDQim2DL0Ebbs'
 
@@ -7,5 +8,14 @@ bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler(commands=['start'])  
 def start_message(message):
     bot.reply_to(message, "Привет, я бесплатный помошник по быстрому поиску музыки!")
+
+@bot.message_handler(commands=['search'])
+def send_music(message):
+    search = message.text.replace("/search ", "")
+    r = requests.get('https://mp3party.net/search?q={}'.format(search))
+    
+    
+
+
 
 bot.infinity_polling()
