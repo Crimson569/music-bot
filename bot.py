60ee3895df3c3c4b73f7b9677e68fd562fb821d7
Tvarkovskiy_Nickolay <80266130+Crimson569@users.noreply.github.com>
2024-12-10 17:32:24 +0200

    Добавлена функция отправки музыки по запросу

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import telebot
 import requests
 
@@ -12,7 +13,25 @@ def start_message(message):
 @bot.message_handler(commands=['search'])
 def send_music(message):
     search = message.text.replace("/search ", "")
+    bot.reply_to(message, "Результаты поиска по запросу {}".format(search))
     r = requests.get('https://mp3party.net/search?q={}'.format(search))
+
+    if r.status_code == 200:
+        soup = BeautifulSoup(r.text, 'html.parser')
+        elements = soup.find_all(class_='track__title js-track-title')
+
+        for element in elements:
+            song_id = element.get('href').replace("/music/", "")
+            r = requests.get('https://dl2.mp3party.net/download/{}'.format(song_id), stream=True)
+            if r.status_code == 200:
+                with open('file.mp3', 'wb') as file:
+                    for chunk in r.iter_content(chunk_size=8192):
+                        file.write(chunk)
+                with open('file.mp3', 'rb') as file:
+                    bot.send_audio(message.chat.id, file)
+
+                del file
+
     
     
 
